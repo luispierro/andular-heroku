c8010936c0194058145d93d44aad9c5af6a73d38
Luis Pierro <luisf.pierro@gmail.com>
2023-11-10 17:54:07 +0000

    deploy angular app to heroku

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-heroku'));
+app.use(express.static('./angular-heroku'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-heroku/src'}),
+    res.sendFile('index.html', {root: '/angular-heroku/src'}),
 );
 
 // Start the app by listening on the default Heroku port
